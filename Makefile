b4ef2564cc7678852284aa0537170a7a6cabbc91
Alfredo Gutiérrez <charecktowa@protonmail.com>
2020-02-01 14:47:17 -0600

    Added the .c headers to Makefile

--- Makefile
+++ Makefile
@@ -9,9 +9,10 @@ LINKER_FLAGS = -lSDL2
 OBJ_NAME = build/game
 
 # headers
-HDR_NAME = src/game.c
+HDR_GAME = src/game.c
+HDR_HNDLR = src/exception.c
 
 all : 
-		$(CC) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME) $(OBJS) $(HDR_NAME)
+		$(CC) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME) $(OBJS) $(HDR_GAME) $(HDR_HNDLR)
 
 # clang-8 -std=c18 -O2 -Wall -Wextra -Wpedantic -v -o game src/main.c
--- src/main.c
+++ src/main.c
@@ -1,57 +1,14 @@
 #include <SDL2/SDL.h>
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+#include "./exception.h"
 #include "./game.h"
 
-int main(int argc, char **argv) {
-  int status; //
-  SDL_Window *window = NULL;
-  SDL_Surface *surface = NULL;
+int status = 0;
 
-  if (init(window, surface, &status) == true)
-    loadMedia(surface, &status); // loads the image for the thin
-
-  // perdón Diosito por usar goto.
-  switch (status) {
-  case NORMAL_EXIT:
-    goto normal;
-    break;
-  case VIDEO_INIT:
-    goto videoErr;
-    break;
-  case WINDOW_INIT:
-    goto windowErr;
-    break;
-  case ASSET_LOAD:
-    goto windowErr;
-    break;
-  default:
-    break; // I do not really know
-  }
-
-  printf("%i\n", status);
-
-  /*Here, all the stuff should load normally, so we can start the game*/
-  SDL_Event e;
-  bool running = true;
-
-  while (running) {
-    while (SDL_PollEvent(&e) != 0) {
-      if (e.type == SDL_QUIT)
-        running = false;
-    }
-  }
-
-normal: // Should end normally closing everything
-
-windowErr:
-  SDL_DestroyWindow(window);
-
-videoErr: // should close only SDL
-  SDL_Quit();
-
-  exit(EXIT_SUCCESS);
+int main(void) {
+        return (initSDL(status));
 }
